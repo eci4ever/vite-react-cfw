b93ca78e0eca899ff99c594f57c5486aa90aa757
eci4ever <eci4ever@gmail.com>
2025-10-14 22:57:29 +0800

    add d1 database

--- src/react-app/routes/index.tsx
+++ src/react-app/routes/index.tsx
@@ -6,10 +6,19 @@ export const Route = createFileRoute("/")({
 });
 
 function Home() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/customers");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching customers:", error);
+    }
+  };
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold underline mb-4">Hello World</h1>
-      <Button>Click me</Button>
+      <Button onClick={fetchData}>Click me</Button>
     </div>
   );
 }
--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,22 @@
 import { Hono } from "hono";
-const app = new Hono<{ Bindings: Env }>();
+
+type Bindings = {
+  d1_vite_react: D1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+
+app.get("/api/customers", async (c) => {
+  try {
+    let { results } = await c.env.d1_vite_react
+      .prepare("SELECT * FROM Customers")
+      .all();
+    return c.json(results);
+  } catch (e) {
+    return c.json({ err: e as string }, 500);
+  }
+});
 
 export default app;
